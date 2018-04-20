9181307789add9256f0f6ef5710bfa874c028c32
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 17:33:48 -0600

    add ImageZone to Proofer

--- src/components/proofer.js
+++ src/components/proofer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TextBox from "./text-box";
+import ImageZone from "./image-zone";
 
 import "./proofer.css";
 
@@ -8,6 +9,7 @@ export default function Proofer(props) {
     return (
         <div className="proofer">
             <TextBox />
+            <ImageZone />
         </div>
     );
 }
