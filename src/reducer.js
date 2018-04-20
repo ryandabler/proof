040e507a396ca0b15efa43a5bc9a4c6d654c4b6c
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 17:28:46 -0600

    reducer now handles LOAD_FILE action

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,13 @@
-import {} from "./actions";
+import { LOAD_FILE } from "./actions";
 
-const initialState = {}
+const initialState = {
+    file: null
+}
 
 export const reducer = (state = initialState, action) => {
+    if (action.type === LOAD_FILE) {
+        return Object.assign({}, state, { file: action.dataURL });
+    }
+
     return state;
 }
