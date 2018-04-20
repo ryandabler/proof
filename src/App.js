edc35426cace3dfb8870cd1e7c6b92ed51a0f4ac
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 16:55:30 -0600

    render Proofer component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
+
+import { Proofer } from "./components/proofer";
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Proofer />
+			</div>
+		);
+	}
 }
 
 export default App;
