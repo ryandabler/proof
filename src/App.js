7de765b8a29c607ade4e9584ffc243dac148d083
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 17:14:15 -0600

    add Sidebar to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Proofer from "./components/proofer";
+import Sidebar from "./components/sidebar";
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Sidebar />
 				<Proofer />
 			</div>
 		);
