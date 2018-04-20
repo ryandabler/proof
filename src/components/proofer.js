c8ae6cf262e09d2bbcf4a80cf357ec3bd73fbb01
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 16:52:57 -0600

    create Proofer component

---
+++ src/components/proofer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { TextBox } from "./text-box";
+
+import "./text-box.css";
+
+export function Proofer(props) {
+    return (
+        <div className="proofer">
+            <TextBox />
+        </div>
+    );
+}
