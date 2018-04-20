05842bc2d2a8fd116ff3ef6355a2205737836bbb
Ryan Dabler <rpdabler@gmail.com>
2018-04-20 17:08:21 -0600

    create Sidebar component

---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./sidebar.css";
+
+export default function Sidebar(props) {
+    return (
+        <div className="sidebar">
+            <button>New</button>
+            <button>View</button>
+        </div>
+    );
+}
