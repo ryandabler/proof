278cc27b4cc8f2745fc550d0f4c391bd795e8d6c
Ryan Dabler <rpdabler@gmail.com>
2018-04-23 14:53:21 -0600

    move .project-overview class to parent element

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -14,8 +14,8 @@ export function LandingPage(props) {
     return (
         <div className="landing-page">
             {projectTiles}
-            <Link to={"/new"} className="plain-link">
-                <div className="project-overview">Create a new Project</div>
+            <Link to={"/new"} className="plain-link project-overview">
+                Create a new Project
             </Link>
         </div>
     );
