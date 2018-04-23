bbc95c591dcad064029a739b00dd6e103d756db8
Ryan Dabler <rpdabler@gmail.com>
2018-04-22 20:35:39 -0600

    fleshed out new project form

--- src/components/new-project-form.js
+++ src/components/new-project-form.js
@@ -5,8 +5,13 @@ import "./new-project-form.css";
 export default function NewProjectForm(props) {
     return (
         <form className="new-project-form">
-            <label htmlFor="project-title">Title</label>
-            <input type="text" id="project-title" name="title" />
+            <label htmlFor="project-title">Name</label>
+            <input type="text" id="project-title" name="title" required />
+            <label htmlFor="project-remote-index">Remote index</label>
+            <input type="text" id="project-remote-index" name="remote" />
+            <label htmlFor="project-file">File (PDF)</label>
+            <input type="file" id="project-file" name="file" />
+            <input type="submit" value="Save" />
         </form>
     );
 }
