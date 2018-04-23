0ba348d709e1d13b24f8ccc5e09dfa56d31a688d
Ryan Dabler <rpdabler@gmail.com>
2018-04-22 20:52:03 -0600

    add submission handler for new project

--- src/components/new-project-form.js
+++ src/components/new-project-form.js
@@ -1,10 +1,30 @@
 import React from "react";
 
+import { extractFormValues } from "../utilities";
+
 import "./new-project-form.css";
 
 export default function NewProjectForm(props) {
+    function submitHandler(e) {
+        e.preventDefault();
+        const formValues = extractFormValues(e.target.elements);
+        
+        if (formValues.file !== "") {
+            const input = e.target.elements.file;
+            const file = input.files[0];
+            const fileReader = new FileReader();
+            let base64;
+
+            fileReader.onload = (loadEvent) => {
+                base64 = loadEvent.target.result;
+            }
+            
+            fileReader.readAsDataURL(file);
+        }
+    }
+
     return (
-        <form className="new-project-form">
+        <form onSubmit={submitHandler} className="new-project-form">
             <label htmlFor="project-title">Name</label>
             <input type="text" id="project-title" name="title" required />
             <label htmlFor="project-remote-index">Remote index</label>
