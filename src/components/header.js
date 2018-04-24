4772c6c1b62771775244b5e0760ac3b1b8ffc214
Ryan Dabler <rpdabler@gmail.com>
2018-04-24 12:08:31 -0600

    remove menu bar from Header

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,6 @@ export default function Header(props) {
     return (
         <header className="header">
             <span className="proof">Proof</span>
-            <ul className="menu">
-                <li>Proofread</li>
-                <li>Help</li>
-            </ul>
         </header>
     );
 }
