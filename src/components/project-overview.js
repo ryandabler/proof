14068cdfad207e1caf4d9cc2f3d84e974763e4e9
Ryan Dabler <rpdabler@gmail.com>
2018-04-24 09:29:44 -0600

    convert tile to link

--- src/components/project-overview.js
+++ src/components/project-overview.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import { Link } from "react-router-dom";
 
 import "./project-overview.css";
 
 export default function ProjectOverview(props) {
     return (
-        <div className="project-overview">
+        <Link to={`/projects/${props.name}`} className="plain-link project-overview">
             <span className="project-name">{props.name}</span>
             <span className="pct">{props.pctComplete}%</span>
-        </div>
+        </Link>
     );
 }
 
