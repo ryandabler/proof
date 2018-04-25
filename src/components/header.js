db1b4ac5dd30790901484653ce1cf6ced870e9df
Ryan Dabler <rpdabler@gmail.com>
2018-04-24 20:45:46 -0600

    convert Proof to link to root

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 import "./header.css";
 
 export default function Header() {
     return (
         <header className="header">
-            <span className="proof">Proof</span>
+            <Link to={"/"} className="plain-link proof">
+                Proof
+            </Link>
         </header>
     );
 }
