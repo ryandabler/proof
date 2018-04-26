d5e64dcb8319bbd82020239997c332ddc17f2a98
Ryan Dabler <rpdabler@gmail.com>
2018-04-25 19:54:49 -0600

    rename ProjectOverview to ProjectTile

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -3,19 +3,19 @@ import { connect } from "react-redux";
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
-import ProjectOverview from "./project-overview";
+import ProjectTile from "./project-tile";
 
 import "./landing-page.css";
 
 export function LandingPage(props) {
     const projectTiles = props.projects.map(project => 
-        <ProjectOverview key={project.name} name={project.name} pctComplete={0} />
+        <ProjectTile key={project.name} name={project.name} pctComplete={0} />
     );
 
     return (
         <div className="landing-page">
             {projectTiles}
-            <Link to={"/new"} className="plain-link project-overview" id="new-project">
+            <Link to={"/new"} className="plain-link project-tile" id="new-project">
                 New Project
             </Link>
         </div>
--- src/components/project-tile.js
+++ src/components/project-tile.js
@@ -2,18 +2,18 @@ import React from "react";
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
-import "./project-overview.css";
+import "./project-tile.css";
 
-export default function ProjectOverview(props) {
+export default function ProjectTile(props) {
     return (
-        <Link to={`/projects/${props.name}`} className="plain-link project-overview">
+        <Link to={`/projects/${props.name}`} className="plain-link project-tile">
             <span className="project-name">{props.name}</span>
             <span className="pct">{props.pctComplete}%</span>
         </Link>
     );
 }
 
-ProjectOverview.propTypes = {
+ProjectTile.propTypes = {
     name: PropTypes.string,
     pctComplete: PropTypes.number
 }
