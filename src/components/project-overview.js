261b92c685a2bdae809a4f7ad75cfba8055b7bda
Ryan Dabler <rpdabler@gmail.com>
2018-04-25 20:44:32 -0600

    create initial layout for ProjectOverview

--- src/components/project-overview.js
+++ src/components/project-overview.js
@@ -5,7 +5,16 @@ import { PropTypes } from "prop-types";
 import "./project-overview.css";
 
 export function ProjectOverview(props) {
-
+    return (
+        <div className="project-overview">
+            <span>Name</span>
+            <span>{props.project.name}</span>
+            <span>Remote</span>
+            <span>{props.project.remote}</span>
+            <span>Pagelist</span>
+            <span></span>
+        </div>
+    );
 }
 
 ProjectOverview.propTypes = {
