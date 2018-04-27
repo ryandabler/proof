61cf4148e1f99cff8b9522027ac8e6486e2e759b
Ryan Dabler <rpdabler@gmail.com>
2018-04-26 20:35:06 -0600

    don't check if file doesn't exist

--- src/components/image-zone.js
+++ src/components/image-zone.js
@@ -5,17 +5,11 @@ import { Document, Page } from "react-pdf";
 import "./image-zone.css";
 
 export default function ImageZone(props) {
-    if (props.file === null) {
-        return (
-            <input type="file" />
-        );
-    } else {
-        return (
-            <Document file={props.file}>
-                <Page pageIndex={props.pageIndex} />
-            </Document>
-        );
-    }
+    return (
+        <Document file={props.file}>
+            <Page pageIndex={props.pageIndex} />
+        </Document>
+    );
 }
 
 ImageZone.propTypes = {
