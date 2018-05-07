3fa1b88ef9375d327e110388af6ac1ef183fb292
Ryan Dabler <rpdabler@gmail.com>
2018-05-07 13:14:22 -0600

    switch open menu when mouse enters another element

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { toggleMenu } from "../utilities";
+import { toggleMenu, hideMenu, showMenu } from "../utilities";
 import { menuItems } from "../config";
 
 import "./menu.css";
@@ -8,8 +8,17 @@ import "./menu.css";
 export default function Menu() {
     const refs = {};
 
+    function onMouseEnter(e) {
+        const menuDisplayed = document.querySelector(".menu-group .menu-dropdown:not(.hidden)");
+        if (menuDisplayed) {
+            hideMenu(menuDisplayed);
+            const newMenuElem = e.target.nextSibling;
+            showMenu(newMenuElem);
+        }
+    }
+
     const menuButtons = menuItems.map(menuGroup => 
-        <div key={menuGroup[0]} className="menu-group">
+        <div key={menuGroup[0]} className="menu-group" onMouseEnter={onMouseEnter}>
             <button className="menu-item" onClick={(e) => toggleMenu(e, menuGroup[0], refs)}>{menuGroup[0]}</button>
             <div className="menu-dropdown hidden" ref={dropdown => refs[menuGroup[0]] = dropdown}>
                 {menuGroup[1].map(item =>
